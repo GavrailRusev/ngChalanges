b3b9156598c8451d0046b0831164f2420c2fd652
GavrailRusev <gavrailrusev@gmail.com>
2025-04-11 06:54:57 +0000

    added to git

---
+++ angular-chalanges-main/.github/github-action/contributors.js
@@ -0,0 +1,32 @@
+const contributors = [
+  'alcaidio',
+  'svenson95',
+  'jdegand',
+  'DeveshChau',
+  'stillst',
+  'wandri',
+  'webbomj',
+  'kabrunko-dev',
+  'Sanjar1304',
+  'tsironis13',
+  'EnochGao',
+];
+
+const sponsors = [
+  'ddotx',
+  'LMFinney',
+  'alannelucq',
+  'SidV2',
+  'fpalmab',
+  'CivilEngeneer',
+  'apalaio',
+  'amosISA',
+  'michalgrzegorczyk-dev',
+  'zealotrahl',
+  'DzoeL123',
+];
+
+module.exports = {
+  contributors,
+  sponsors,
+};
